aa3f814010e04734824a15b8b0024d1b73e19285
Sebastian Velasco <sebasvelasco353@users.noreply.github.com>
2021-11-12 23:42:02 -0500

    u3 utility functions

---
+++ sketch-02/sketch-02.js
@@ -0,0 +1,46 @@
+const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+
+    const cx = width * 0.5;
+    const cy = height * 0.5;
+
+    const w = width * 0.01;
+    const h = height * 0.1;
+
+    const num = 12;
+    const radius = width * 0.3;
+
+    for (let i = 0; i < num; i++) {
+      const slice = math.degToRad(360 / num);
+      const angle = slice * i;
+
+      const x = cx + radius * Math.sin(angle);
+      // const y = height * 0.5;
+      const y = cy + radius * Math.cos(angle);
+
+      context.save();
+      context.translate(x, y);
+      context.rotate(-angle);
+      context.scale(random.range(1, 3), 1);
+
+      context.beginPath();
+      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.fill();
+      context.restore();
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
