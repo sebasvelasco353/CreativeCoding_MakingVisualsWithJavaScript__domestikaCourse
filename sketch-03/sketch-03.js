06ed5a39bef9e8f78e4d27ca841ba3d13eba2b8d
Sebastian Velasco <sebasvelasco353@users.noreply.github.com>
2021-11-14 23:28:27 -0500

    u4 v3

--- sketch-03/sketch-03.js
+++ sketch-03/sketch-03.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
@@ -20,6 +21,22 @@ const sketch = ({ context, width, height }) => {
     context.fillRect(0, 0, width, height);
 
     for (let i = 0; i < agents.length; i++) {
+      const agent = agents[i];
+      for (let j = i + 1; j < agents.length; j++) {
+        const other = agents[j];
+
+        const dist = agent.pos.getDistance(other.pos);
+        if (dist > 200) continue;
+        context.lineWidth = math.mapRange(dist, 0, 200, 5, 1);
+
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+      }
+    }
+
+    for (let i = 0; i < agents.length; i++) {
       const element = agents[i];
       element.update();
       element.draw(context);
@@ -34,6 +51,12 @@ class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+  }
+
+  getDistance(v) {
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+    return Math.sqrt(dx * dx + dy + dy);
   }
 }
 
