a63d5493a97440ca2970b2f2e9b418eb08bbe3a5
Sebastian Velasco <sebasvelasco353@users.noreply.github.com>
2021-11-15 12:16:45 -0500

    u6 v1

---
+++ sketch-05/sketch-05.js
@@ -0,0 +1,36 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+let text = 'S';
+let fontSize = 1200;
+let fontFamily = 'serif';
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+    context.font = `${fontSize}px ${fontFamily}`;
+    context.textBaseline = 'top';
+
+    const metrics = context.measureText(text);
+    const mx = metrics.actualBoundingBoxLeft * -1;
+    const my = metrics.actualBoundingBoxAscent * -1;
+    const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+    const x = (width - mw) * 0.5 - mx;
+    const y = (height - mh) * 0.5 - my;
+
+    context.save();
+    context.translate(x, y);
+    context.fillText(text, 0, 0);
+    context.restore();
+  };
+};
+
+canvasSketch(sketch, settings);
