d54147d3247aab5346b2d1b617cd33549a9e3859
Sebastian Velasco <sebasvelasco353@users.noreply.github.com>
2021-11-15 12:30:41 -0500

    u6 v2

--- sketch-05/sketch-05.js
+++ sketch-05/sketch-05.js
@@ -4,6 +4,7 @@ const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
+let manager;
 let text = 'S';
 let fontSize = 1200;
 let fontFamily = 'serif';
@@ -33,4 +34,14 @@ const sketch = () => {
   };
 };
 
-canvasSketch(sketch, settings);
+const onKeyUp = (e) => {
+  text = e.key.toUpperCase();
+  manager.render();
+}
+
+document.addEventListener('keyup', onKeyUp);
+
+const start = async () => {
+  manager = await canvasSketch(sketch, settings);
+}
+start();
