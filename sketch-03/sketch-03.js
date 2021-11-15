bb3d2126e17dbf363fa95fabb4958164a3974bf2
Sebastian Velasco <sebasvelasco353@users.noreply.github.com>
2021-11-14 23:18:49 -0500

    u4 vid 2

--- sketch-03/sketch-03.js
+++ sketch-03/sketch-03.js
@@ -2,7 +2,8 @@ const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
 
 const sketch = ({ context, width, height }) => {
@@ -20,15 +21,17 @@ const sketch = ({ context, width, height }) => {
 
     for (let i = 0; i < agents.length; i++) {
       const element = agents[i];
+      element.update();
       element.draw(context);
+      element.bounce(width, height);
     }
   };
 };
 
 canvasSketch(sketch, settings);
 
-class Point {
-  constructor(x, y, radius) {
+class Vector {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
   }
@@ -36,14 +39,32 @@ class Point {
 
 class Agent {
   constructor(x, y) {
-    this.pos = new Point(x, y);
-    this.radius = 10;
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = random.range(4, 12);
+  }
+
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
+  }
+
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
   }
 
   draw(context) {
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+
+    context.lineWidth = 4;
+
     context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
-    context.fillStyle = 'black';
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
+    context.stroke();
+
+    context.restore();
   }
 }
