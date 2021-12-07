02b8409dd1260b223372d776d34656728785c840
Sebastian Velasco <sebasvelasco353@users.noreply.github.com>
2021-12-07 18:55:36 -0500

    changed return in line 59 of sketch 03, pepeudotcom made me realize via an issue there was a typo

--- sketch-03/sketch-03.js
+++ sketch-03/sketch-03.js
@@ -56,7 +56,7 @@ class Vector {
   getDistance(v) {
     const dx = this.x - v.x;
     const dy = this.y - v.y;
-    return Math.sqrt(dx * dx + dy + dy);
+    return Math.sqrt(dx * dx + dy * dy);
   }
 }
 
